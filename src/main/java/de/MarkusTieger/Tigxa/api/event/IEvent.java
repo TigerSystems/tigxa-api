c4dd901a95bcb9b07d764428dfb994e3fc65e353
MarkusTieger <markustieger@gmail.com>
2022-04-12 23:50:32 +0200

    Added Permission-System to Events

--- src/main/java/de/MarkusTieger/Tigxa/api/event/IEvent.java
+++ src/main/java/de/MarkusTieger/Tigxa/api/event/IEvent.java
@@ -1,7 +1,11 @@
 package de.MarkusTieger.Tigxa.api.event;
 
+import de.MarkusTieger.Tigxa.api.permission.Permission;
+
 public interface IEvent {
 
     public String getName();
+
+    public Permission[] getRequiredPermissions();
 
 }
